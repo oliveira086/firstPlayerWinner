22d09ccd5590ebe2cb1f7d7529a92e32489f6f90
Alessandro <alessandroadm@live.com>
2021-01-02 15:51:07 -0300

    feat: define entity attributes types

--- src/auth/User.entity.ts
+++ src/auth/User.entity.ts
@@ -1,5 +1,6 @@
 import {
   BaseEntity,
+  BeforeInsert,
   Column,
   CreateDateColumn,
   Entity,
@@ -7,47 +8,55 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { BcryptUtil } from './bcrypt-utils';
+import { userPermission } from './userPermission';
+import { format } from 'date-fns';
 
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
-  @Column({ unique: true, nullable: false })
+  @Column({ unique: true, nullable: false, length: 120 })
   email!: string;
 
-  @Column({ nullable: false })
+  @Column({ nullable: false, type: 'varchar', length: 255 })
   password!: string;
 
-  @Column({ name: 'eletronic_signature' })
+  @Column({ name: 'eletronic_signature', type: 'varchar', nullable: true })
   eletronicSignature!: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 70 })
   name!: string;
 
-  @Column()
+  @Column({ type: 'enum', enum: userPermission, default: userPermission.USER })
   permission!: string;
 
-  @Column({ name: 'last_access' })
+  @Column({
+    type: 'datetime',
+    name: 'last_access',
+    nullable: false,
+    default: format(new Date(), 'yyyy-MM-dd H:mm:ss'),
+  })
   lastAccess!: Date;
 
-  @Column()
+  @Column({ type: 'int', nullable: false, default: 0 })
   balance!: number;
 
-  @Column()
+  @Column({ type: 'varchar', nullable: true })
   pixKey!: string;
 
-  @CreateDateColumn({ name: 'created_at' })
+  @CreateDateColumn({ name: 'created_at', nullable: false })
   createdAt!: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
+  @UpdateDateColumn({ name: 'updated_at', nullable: false })
   updatadAt!: Date;
 
   async validatePassword(textPassword: string): Promise<boolean> {
     return BcryptUtil.init().comparePassword(textPassword, this.password);
   }
 
-  async encriptPassword(textPassword: string): Promise<void> {
-    this.password = await BcryptUtil.init().encriptPassword(textPassword);
+  @BeforeInsert()
+  async encriptPassword(): Promise<void> {
+    this.password = await BcryptUtil.init().encriptPassword(this.password);
   }
 }
